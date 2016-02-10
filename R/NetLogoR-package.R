a9296c4f82d79161b21b2174f0f774c69a27ee04
Alex Chubaty <alex.chubaty@gmail.com>
2016-02-10 09:47:27 -0700

    fixes and cleanup
    * code formatting
    * add `raster` to Depends (required *e.g.*, for plotting)
    * import `methods`
    * add sp to Imports (we'll need it later)
    * add `knitr` and `testthat` to Suggests
    * add `tests/` dir
    * add `vignettes/` dir
    * redoc

---
+++ R/NetLogoR-package.R
@@ -0,0 +1,3 @@
+#' @import methods
+#' @import raster
+NULL
--- R/createWorld-function.R
+++ R/createWorld-function.R
@@ -29,20 +29,23 @@
 #'             Center for Connected Learning and Computer-Based Modeling,
 #'             Northwestern University. Evanston, IL.
 #'
-#' @examples # Create a square world of 25 patches.
-#'           world <- createWorld(minPxcor = 0, maxPxcor = 5, minPycor = 0, maxPycor = 5)
-#'           # Give the patches random values between 0 and 10.
-#'           world[] <- runif(n = 25, min = 0, max = 10)
-#'           plot(world)
+#' @examples
+#' # Create a square world of 25 patches.
+#' world <- createWorld(minPxcor = 0, maxPxcor = 5, minPycor = 0, maxPycor = 5)
+#' # Give the patches random values between 0 and 10.
+#' world[] <- runif(n = 25, min = 0, max = 10)
+#' plot(world)
 #'
 #' @export
-#' @importFrom raster raster
 #' @docType methods
 #' @rdname createWorld
 #'
 #' @author Sarah Bauduin
 #'
-setGeneric("createWorld", function(minPxcor = -16, maxPxcor = 16, minPycor = -16, maxPycor = 16, patchSize = 1, ...){
+setGeneric(
+  "createWorld",
+  function(minPxcor = -16, maxPxcor = 16, minPycor = -16, maxPycor = 16,
+           patchSize = 1, ...) {
   standardGeneric("createWorld")
 })
 
@@ -52,8 +55,10 @@ setMethod(
   "createWorld",
   #signature = c("numeric", "numeric", "numeric", "numeric", "numeric"), # not working
   #signature = c("numeric", "numeric", "numeric", "numeric", "numeric", ...), # not working
-  definition = function(minPxcor = -16, maxPxcor = 16, minPycor = -16, maxPycor = 16, patchSize = 1, ...){
-    worldRaster<-raster(xmn=minPxcor, xmx=maxPxcor, ymn=minPycor, ymx=maxPycor, res=patchSize, ...)
+  definition = function(minPxcor = -16, maxPxcor = 16, minPycor = -16,
+                        maxPycor = 16, patchSize = 1, ...) {
+    worldRaster <- raster(xmn = minPxcor, xmx = maxPxcor, ymn = minPycor,
+                          ymx = maxPycor, res = patchSize, ...)
     return(worldRaster)
   }
 )
