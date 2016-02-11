51459aa6ac9c49b699e082a4932a89cde76f1eff
SarahBauduin <sarahbauduin@hotmail.fr>
2016-02-11 10:24:12 -0500

    [NetLogoR] createWorld test default values

--- tests/testthat/test-createWorld.R
+++ tests/testthat/test-createWorld.R
@@ -4,3 +4,10 @@ test_that("createWorld handles different inputs", {
 
   expect_identical(w1, w2)
 })
+
+test_that("createWorld works with default values", {
+  w1 <- createWorld()
+  w2 <- createWorld(-16, 16, -16, 16, 1) # default values
+
+  expect_identical(w1, w2)
+})
