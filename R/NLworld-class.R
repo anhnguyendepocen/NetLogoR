d9c984ae6f24ce41b0b36a1a310b6e82365d71d9
SarahBauduin <sarahbauduin@hotmail.fr>
2016-02-24 14:41:23 -0500

    [NetLogoR] [ ] method for NLworld and NLworldStack

--- R/NLworld-class.R
+++ R/NLworld-class.R
@@ -3,21 +3,24 @@
 #'
 #' Behaves the same as a \code{RasterLayer} object except:
 #'
-#' A \code{NLworld} is a grid composed of squared patches of resolution 1.
-#' Patches have two coordinates \code{pxcor} and \code{pycor}.
-#' \code{pxcor} and \code{pycor} are always integer.
-#' \code{pxcor} and \code{pycor} represent the raster coordinates in the bottom left
-#' corner of the patches.
-#' When creating a \code{NLworld}, the extent of the \code{NLworld} is of
+#' A \code{NLworld} is a grid composed of squared patches.
+#' Patches have two coordinates \code{pxcor} and \code{pycor}, considered as the
+#' coordinates of their center.
+#' \code{pxcor} and \code{pycor} are always integer and increment by 1.
+#' When creating a new \code{NLworld}, the extent of the \code{NLworld} is of
 #' \code{xmin = minPxcor}, \code{xmax = maxPxcor}, \code{ymin = minPycor}, and
 #' \code{ymax = maxPycor}. The number of patches is equal to
 #' \code{((maxPxcor - minPxcor) + 1) * ((maxPycor - minPycor) + 1)}.
 #' \code{pxcor} can be seen as a column number and \code{pycor} can be seen as a
 #' row number but increasing as you move up. However, \code{pxcor} and \code{pycor}
 #' can be negative if there are patches to the left or below the patch \code{[0,0]}.
-#' The use of \code{[]} to extract \code{Raster*} cells by row and column numbers has
-#' been redefined to extract \code{NLworld} patches using the patches' coordinates
-#' \code{[pxcor,pyxor]}.
+#'
+#' The use of \code{[]} to extract \code{Raster*} cell values by row and column numbers
+#' has been redefined to extract \code{NLworld} patches using the patches' coordinates
+#' \code{[pxcor,pyxor]}. When multiple coordinates are provided, the order of the
+#' values returned matches the order of the cell numbers as defined for a \code{Raster*}.
+#' Similarly, when replacing values of several patches using \code{[]<-}, the values
+#' will be given in the order of the cell numbers as defined for a \code{Raster*}.
 #'
 #' @inheritParams RasterLayer
 #'
@@ -41,6 +44,39 @@ setClass(
   )
 )
 
+#' @export
+#' @name [
+#' @docType methods
+#' @rdname NLworld-class
+setMethod(
+  "[",
+  signature("NLworld", "numeric", "numeric", "ANY"),
+  definition = function(x, i, j, drop) {
+
+    cells <- which(x@pxcor %in% i & x@pycor %in% j, TRUE) # cell number(s)
+    xValues <- values(x)
+    cellValues <- xValues[cells]
+
+    return(cellValues)
+  }
+)
+
+#' @export
+#' @name [<-
+#' @rdname NLworld-class
+setReplaceMethod(
+  "[",
+  signature("NLworld","numeric","numeric","numeric"),
+  definition = function(x, i, j, value) {
+
+    cells <- which(x@pxcor %in% i & x@pycor %in% j, TRUE) # cell number(s)
+    x@data@values[cells] <- value
+
+    validObject(x)
+    return(x)
+  }
+)
+
 
 ################################################################################
 #' The \code{NLworldStack} class
@@ -56,6 +92,77 @@ setClass(
 setClass(
   "NLworldStack",
   contains = "RasterStack"
+)
+
+#' @export
+#' @name [
+#' @docType methods
+#' @rdname NLworldStack-class
+setMethod(
+  "[",
+  signature("NLworldStack", "numeric", "numeric", "ANY"),
+  definition = function(x, i, j, drop) {
+
+    x_l <- x[[1]]
+    cells <- which(x_l@pxcor %in% i & x_l@pycor %in% j, TRUE) # same cell number(s) for all layers
+    xValues <- values(x)
+    cellValues <- xValues[cells,]
+
+    return(cellValues)
+  }
+)
+
+#' @export
+#' @name [<-
+#' @rdname NLworldStack-class
+setReplaceMethod(
+  "[",
+  signature("NLworldStack","numeric","numeric","matrix"),
+  definition = function(x, i, j, value) {
+
+    x_l <- x[[1]]
+    cells <- which(x_l@pxcor %in% i & x_l@pycor %in% j, TRUE) # same cell number(s) for all layers
+    xValues <- values(x)
+    xValues[cells,] <- value
+
+    for(k in 1:nlayers(x)){
+      x <- setValues(x, values = xValues[,k], layer = k) # replace values of each layer
+    }
+
+    validObject(x)
+    return(x)
+  }
+)
+
+#' @export
+#' @name [<-
+#' @rdname NLworldStack-class
+setReplaceMethod(
+  "[",
+  signature("NLworldStack","numeric","numeric","numeric"),
+  definition = function(x, i, j, value) {
+
+    value <- matrix(data = value, nrow = 1, ncol = length(value))
+
+    # Copy/paste of above. Putting :
+    # x[i,j] <- value
+    # did not work
+
+    x_l <- x[[1]]
+    cells <- which(x_l@pxcor %in% i & x_l@pycor %in% j, TRUE) # same cell number(s) for all layers
+    xValues <- values(x)
+    xValues[cells,] <- value
+
+    for(k in 1:nlayers(x)){
+      x <- setValues(x, values = xValues[,k], layer = k) # replace values of each layer
+    }
+
+    validObject(x)
+    return(x)
+
+    # end copy/paste from above
+
+  }
 )
 
 
--- tests/testthat/test-NLworld-class.R
+++ tests/testthat/test-NLworld-class.R
@@ -15,3 +15,38 @@ test_that("NLstack() works similar as stack()",{
 
   expect_identical(sNL, ws)
 })
+
+test_that("[] works with NLworld and pxcor and pycor",{
+  w1 <- createNLworld(minPxcor = 0, maxPxcor = 1, minPycor = 0, maxPycor = 1)
+  w1[] <- c(1, 2, 3, 4)
+
+  w1_00 <- w1[0,0]
+  expect_identical(w1_00, 3)
+
+  w1_01_11 <- w1[c(0, 1),1]
+  expect_identical(w1_01_11, c(1, 2))
+
+  w1[1,c(0,1)] <- c(10, 20)
+  expect_identical(values(w1)[c(2, 4)], c(10, 20))
+})
+
+test_that("[] works with NLworldStack and paxcor and pycor",{
+  w1 <- createNLworld(minPxcor = 0, maxPxcor = 1, minPycor = 0, maxPycor = 1)
+  w1[] <- c(1, 2, 3, 4)
+  w2 <- w1
+  w2[] <- c(10, 20, 30, 40)
+  ws <- NLstack(w1, w2)
+
+  ws_00 <- ws[0,0]
+  expect_identical(as.numeric(ws_00), c(3,30))
+
+  ws_01_11 <- ws[c(0, 1),1]
+  expect_identical(ws_01_11, cbind(w1 = c(1, 2), w2 = c(10, 20)))
+
+  ws[1,c(0,1)] <- cbind(c(10, 20), c(100, 200)) # signature matrix for value
+  expect_identical(values(ws)[c(2, 4),], cbind(w1 = c(10, 20), w2 = c(100, 200)))
+
+  ws[1,1] <- c(-1, -4) # signature numeric for value
+  expect_identical(as.numeric(values(ws)[2,]), c(-1, -4))
+})
+
