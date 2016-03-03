cca1dd9c980348bb2c8500d2b0750a6933c9c7f1
SarahBauduin <sarahbauduin@hotmail.fr>
2016-03-03 11:02:34 -0500

    [NetLogoR] new: sortOn(), NLwith() + tests

--- R/agentset-functions.R
+++ R/agentset-functions.R
@@ -1,4 +1,151 @@
 ################################################################################
+#' Sort on
+#'
+#' Reports the coordinates \code{pxcor} and \code{pycor} of the patches sorted
+#' according to their value.
+#'
+#'!!! Only implemented for patches so far !!!
+#'
+#' @param world  A \code{NLworld*} object.
+#'
+#' @param agents A matrix (ncol = 2) with the first column \code{pxcor} and the
+#'               second column \code{pycor} representing the coordinates of the
+#'               patches to be sorted.
+#'
+#' @param pVar   If the world is a \code{NLworldStack}, pVar is the name (characters)
+#'               of the layer used for sorting the patches.
+#'
+#' @return A matrix (ncol = 2) with the first column \code{pxcor} and the second column
+#'         \code{pycor} representing the coordinates of the sorted patches.
+#'
+#' @details The sorting of the agents based on their value is done in a increasing order.
+#'
+#' @references Wilensky, U. 1999. NetLogo. http://ccl.northwestern.edu/netlogo/.
+#'             Center for Connected Learning and Computer-Based Modeling,
+#'             Northwestern University. Evanston, IL.
+#'
+#' @examples
+#' w1 <- createNLworld(minPxcor = 0, maxPxcor = 4, minPycor = 0, maxPycor = 4)
+#' w1[] <- sample(1:5, size = 25, replace = TRUE)
+#' plot(w1)
+#' p1 <- sortOn(world = w1, agents = patches(world = w1))
+#'
+#' @export
+#' @docType methods
+#' @rdname sortOn
+#'
+#' @author Sarah Bauduin
+#'
+setGeneric(
+  "sortOn",
+  function(world, agents, pVar) {
+    standardGeneric("sortOn")
+  })
+
+#' @export
+#' @rdname sortOn
+setMethod(
+  "sortOn",
+  signature = c("NLworld", "matrix", "missing"),
+  definition = function(world, agents) {
+    values <- values(world)
+    pxcorW <- world@pxcor
+    pycorW <- world@pycor
+    agentsVal <- values[pxcorW == agents[,1] & pycorW == agents[,2]]
+    pCoords <- agents[order(agentsVal),]
+    return(pCoords)
+  }
+)
+
+#' @export
+#' @rdname sortOn
+setMethod(
+  "sortOn",
+  signature = c("NLworldStack", "matrix", "character"),
+  definition = function(world, agents, pVar) {
+    names_l <- names(world)
+    l <- match(pVar, names_l)
+    world_l <- world[[l]]
+    sortOn(world = world_l, agents = agents)
+  }
+)
+
+################################################################################
+#' NLwith
+#'
+#' Reports the coordinates \code{pxcor} and \code{pycor} of the patches with their
+#' variable equals to a specific value.
+#'
+#'!!! Only implemented for patches so far !!!
+#'
+#' @param world  A \code{NLworld*} object.
+#'
+#' @param agents A matrix (ncol = 2) with the first column \code{pxcor} and the
+#'               second column \code{pycor} representing the coordinates of the
+#'               patches to be evaluated.
+#'
+#' @param pVar   If the world is a \code{NLworldStack}, pVar is the name (characters)
+#'               of the layer used for evaluating the conditional value.
+#'
+#' @param val    Numeric or character depending on the variable class.
+#'
+#' @return A matrix (ncol = 2) with the first column \code{pxcor} and the second column
+#'         \code{pycor} representing the coordinates of the patches with their value
+#'         equal to \code{val}.
+#'
+#' @references Wilensky, U. 1999. NetLogo. http://ccl.northwestern.edu/netlogo/.
+#'             Center for Connected Learning and Computer-Based Modeling,
+#'             Northwestern University. Evanston, IL.
+#'
+#' @examples
+#' w1 <- createNLworld(minPxcor = 0, maxPxcor = 4, minPycor = 0, maxPycor = 4)
+#' w1[] <- sample(1:5, size = 25, replace = TRUE)
+#' p2 <- NLwith(world = w1, agents = patches(world = w1), val = 2)
+#' plot(w1)
+#'
+#' @export
+#' @docType methods
+#' @rdname NLwith
+#'
+#' @author Sarah Bauduin
+#'
+setGeneric(
+  "NLwith",
+  function(world, agents, pVar, val) {
+    standardGeneric("NLwith")
+  })
+
+#' @export
+#' @rdname NLwith
+setMethod(
+  "NLwith",
+  signature = c("NLworld", "matrix", "missing", "numeric"),
+  definition = function(world, agents, val) {
+    pxcor <- agents[,1]
+    pycor <- agents[,2]
+    values <- world[pxcor,pycor]
+    pVal <- which(values %in% val)
+    pxcorVal <- pxcor[pVal]
+    pycorVal <- pycor[pVal]
+    return(cbind(pxcor = pxcorVal, pycor = pycorVal))
+  }
+)
+
+#' @export
+#' @rdname NLwith
+setMethod(
+  "NLwith",
+  signature = c("NLworldStack", "matrix", "character", "numeric"),
+  definition = function(world, agents, pVar, val) {
+    names_l <- names(world)
+    l <- match(pVar, names_l)
+    world_l <- world[[l]]
+    NLwith(world = world_l, agents = agents, val = val)
+  }
+)
+
+
+################################################################################
 #' With maximum
 #'
 #' Reports the coordinates \code{pxcor} and \code{pycor} of the patches which
--- tests/testthat/test-agentset-functions.R
+++ tests/testthat/test-agentset-functions.R
@@ -1,3 +1,36 @@
+test_that("sortOn works",{
+  w1 <- createNLworld(0, 4, 0, 4)
+  w1[] <- 25:1
+  p1 <- sortOn(world = w1, agents = patches(world = w1))
+  expect_equivalent(cbind(p1[1,1], p1[1,2]), patch(w1, xcor = 4, ycor = 0))
+  expect_equivalent(cbind(p1[25,1],p1[25,2]), patch(w1, xcor = 0, ycor = 4))
+
+  w2 <- w1
+  w2[] <- 1:25
+  ws <- NLstack(w1, w2)
+  p1 <- sortOn(world = ws, agents = patch(world = ws, xcor = c(0,1,2,3,4), ycor = c(4,4,4,4,4)), pVar = "w1")
+  p2 <- sortOn(world = ws, agents = patch(world = ws, xcor = c(0,1,2,3,4), ycor = c(4,4,4,4,4)), pVar = "w2")
+  expect_equivalent(p1, cbind(pxcor = c(4,3,2,1,0), pycor = c(4,4,4,4,4)))
+  expect_equivalent(p2, cbind(pxcor = c(0,1,2,3,4), pycor = c(4,4,4,4,4)))
+})
+
+test_that("NLwith works",{
+  w1 <- createNLworld(0, 4, 0, 4)
+  w1[] <- 0
+  w1[1,c(1,4)] <- c(1,1)
+  p1 <- NLwith(world = w1, agents = patches(world = w1), val = 1)
+  expect_identical(p1, patch(w1, xcor = c(1,1), ycor = c(4,1)))
+
+  w2 <- w1
+  w2[] <- 0
+  w2[2,c(2,4)] <- c(1,1)
+  ws <- NLstack(w1, w2)
+  p1w1 <- NLwith(world = ws, agents = patches(world = ws), pVar = "w1", val = 1)
+  p1w2 <- NLwith(world = ws, agents = patches(world = ws), pVar = "w2", val = 1)
+  expect_identical(p1w1, patch(ws, xcor = c(1,1), ycor = c(4,1)))
+  expect_identical(p1w2, patch(ws, xcor = c(2,2), ycor = c(4,2)))
+})
+
 test_that("withMax works",{
   w1 <- createNLworld(0, 4, 0, 4)
   w1[] <- runif(25)
